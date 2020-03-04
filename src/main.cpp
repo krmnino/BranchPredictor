a003065d96cdc6f9098992b9156273dab6b0a773
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2020-03-03 20:30:53 -0500

    wrote program skeleton

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+void all_taken(){
+
+}
+
+void all_not_taken(){
+
+}
+
+void bimodal_1bit(){
+
+}
+
+void bimodal_2bit(){
+
+} 
+
+void gshare(){
+
+}
+
+void tournament(){
+
+}
+
+void branch_target_buffer(){
+    
+}
+
+int main(int argc, char *argv[]) {
+
+  unsigned long long addr;
+  string behavior;
+  unsigned long long target;
+  ifstream infile("test_input.txt");
+
+  while(infile >> std::hex >> addr >> behavior >> std::hex >> target) {
+    
+  }
+
+  return 0;
+}
