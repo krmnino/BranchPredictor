0c7605231fce1a5015ea3cc2db42332cabe7bc81
krmnino <kurt.manrique.n@gmail.com>
2020-03-04 12:30:47 -0500

    implemented bimodal_2bit() but output does not match with the provided output file, needs to be fixed

--- src/predictions.cpp
+++ src/predictions.cpp
@@ -6,127 +6,131 @@
 
 using namespace std;
 
-void all_taken(string file_name){
-  int correct = 0;
-  int total = 0;
-  unsigned long long addr;
-  string behavior;
-  unsigned long long target;
-  ifstream infile(file_name);
-  while(infile >> std::hex >> addr >> behavior >> std::hex >> target) {
-    if(behavior == "T"){
-      correct++;
-    }
-    total++;
-  }
-  infile.close();
-  cout << (double)correct / total * 100 << "%" << endl;
+void all_taken(string file_name) {
+	int correct = 0;
+	int total = 0;
+	unsigned long long addr;
+	string behavior;
+	unsigned long long target;
+	ifstream infile(file_name);
+	while (infile >> std::hex >> addr >> behavior >> std::hex >> target) {
+		if (behavior == "T") {
+			correct++;
+		}
+		total++;
+	}
+	infile.close();
+	cout << correct << "," << total << "; ";
 }
 
-void all_not_taken(string file_name){
-  int correct = 0;
-  int total = 0;
-  unsigned long long addr;
-  string behavior;
-  unsigned long long target;
-  ifstream infile(file_name);
-  while(infile >> std::hex >> addr >> behavior >> std::hex >> target) {
-    if(behavior == "NT"){
-      correct++;
-    }
-    total++;
-  }
-  infile.close();
-  cout << (double)correct / total * 100 << "%" << endl;
+void all_not_taken(string file_name) {
+	int correct = 0;
+	int total = 0;
+	unsigned long long addr;
+	string behavior;
+	unsigned long long target;
+	ifstream infile(file_name);
+	while (infile >> std::hex >> addr >> behavior >> std::hex >> target) {
+		if (behavior == "NT") {
+			correct++;
+		}
+		total++;
+	}
+	infile.close();
+	cout << correct << "," << total << "; ";
 }
 
-void bimodal_1bit(string file_name){
-  int correct = 0;
-  int total = 0;
-  for(int i = 4; i < 12; i++){
-    if(i == 6){
-      continue;
-    }
-    int table_size = pow(2, i);
-    vector<short> table;
-    table.resize(table_size, 1);
-    correct = 0;
-    total = 0;
-    unsigned long long addr;
-    string behavior;
-    unsigned long long target;
-    ifstream infile(file_name);
-    while(infile >> std::hex >> addr >> behavior >> std::hex >> target){
-      if(table[addr % table_size] == 1 && behavior == "NT"){
-        table[addr % table_size] = 0;
-      }
-      else if(table[addr % table_size] == 0 && behavior == "T"){
-        table[addr % table_size] = 1;
-      }
-      else{
-        correct++;
-      }
-      total++;
-    }
-    cout << "Table size: " << table_size << "---" << (double)correct / total * 100 << "%" << endl;
-    infile.close();
-  }  
-} 
-
-void bimodal_2bit(string file_name){
-  int correct = 0;
-  int total = 0;
-  for(int i = 4; i < 12; i++){
-    if(i == 6){
-      continue;
-    }
-    int table_size = pow(2, i);
-    vector<short> table;
-    table.resize(table_size, 3);
-    correct = 0;
-    total = 0;
-    unsigned long long addr;
-    string behavior;
-    unsigned long long target;
-    ifstream infile(file_name);
-    while(infile >> std::hex >> addr >> behavior >> std::hex >> target){
-      if(0 < table[addr % table_size] && table[addr % table_size] <= 3 && behavior == "NT"){
-        table[addr % table_size]--;
-      }
-      if(0 < table[addr % table_size] && table[addr % table_size] <= 3 && behavior == "T"){
-        table[addr % table_size]--;
-      }
-      total++;
-    }
-    cout << "Table size: " << table_size << "---" << (double)correct / total * 100 << "%" << endl;
-    infile.close();
-  }  
+void bimodal_1bit(string file_name) {
+	int correct = 0;
+	int total = 0;
+	for (int i = 4; i < 12; i++) {
+		if (i == 6) {
+			continue;
+		}
+		int table_size = pow(2, i);
+		vector<short> table;
+		table.resize(table_size, 1);
+		correct = 0;
+		total = 0;
+		unsigned long long addr;
+		string behavior;
+		unsigned long long target;
+		ifstream infile(file_name);
+		while (infile >> std::hex >> addr >> behavior >> std::hex >> target) {
+			if (table[addr % table_size] == 1 && behavior == "NT") {
+				table[addr % table_size] = 0;
+			}
+			else if (table[addr % table_size] == 0 && behavior == "T") {
+				table[addr % table_size] = 1;
+			}
+			else {
+				correct++;
+			}
+			total++;
+		}
+		cout << correct << "," << total << "; ";
+		infile.close();
+	}
 }
 
-void gshare(){
-
+void bimodal_2bit(string file_name) {
+	int correct = 0;
+	int total = 0;
+	for (int i = 4; i < 12; i++) {
+		if (i == 6) {
+			continue;
+		}
+		int table_size = pow(2, i);
+		vector<short> table;
+		table.resize(table_size, 3);
+		correct = 0;
+		total = 0;
+		unsigned long long addr;
+		string behavior;
+		unsigned long long target;
+		ifstream infile(file_name);
+		while (infile >> std::hex >> addr >> behavior >> std::hex >> target) {
+			if (0 <= table[addr % table_size] && table[addr % table_size] <= 3 && behavior == "NT") {
+				table[addr % table_size]--;
+			}
+			else if (0 <= table[addr % table_size] && table[addr % table_size] <= 3 && behavior == "T") {
+				table[addr % table_size]++;
+			}
+			else {
+				correct++;
+			}
+			total++;
+		}
+		cout << correct << "," << total << "; ";
+		infile.close();
+	}
 }
 
-void tournament(){
-
-}
-
-void branch_target_buffer(){
+void gshare() {
 
 }
 
-void test_mod(string file_name){
-  for(int i = 4; i < 12; i++){
-    if(i == 6) continue;
-    cout << pow(2, i) << endl;
-  }
+void tournament() {
+
 }
 
-int main(int argc, char *argv[]) {
-  string name = "../project1/traces/short_trace1.txt";
-  //all_taken(name);
-  //all_not_taken(name);
-  bimodal_1bit(name);
-  //test_mod(name);
-  return 0;
+void branch_target_buffer() {
+
+}
+
+void test_mod(string file_name) {
+	for (int i = 4; i < 12; i++) {
+		if (i == 6) continue;
+		cout << pow(2, i) << endl;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	string name = "short_trace1.txt";
+	//all_taken(name);
+	//all_not_taken(name);
+	//bimodal_1bit(name);
+	bimodal_2bit(name);
+	//test_mod(name);
+	return 0;
 }
