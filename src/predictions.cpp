96eb7249aca6e6e1c7c3861b9641b91ae1317ede
krmnino <kurt.manrique.n@gmail.com>
2020-03-04 19:24:38 -0500

    implemented gshare()

--- src/predictions.cpp
+++ src/predictions.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cmath>
 
+#include <bitset>
+
 using namespace std;
 
 void all_taken(string file_name) {
@@ -41,8 +43,8 @@ void all_not_taken(string file_name) {
 }
 
 void bimodal_1bit(string file_name) {
-	int correct = 0;
-	int total = 0;
+	int correct;
+	int total;
 	for (int i = 4; i < 12; i++) {
 		if (i == 6) {
 			continue;
@@ -74,8 +76,8 @@ void bimodal_1bit(string file_name) {
 }
 
 void bimodal_2bit(string file_name) {
-	int correct = 0;
-	int total = 0;
+	int correct;
+	int total;
 	for (int i = 4; i < 11; i++) {
 		if (i == 6) {
 			continue;
@@ -90,14 +92,14 @@ void bimodal_2bit(string file_name) {
 		unsigned long long target;
 		ifstream infile(file_name);
 		while (infile >> std::hex >> addr >> behavior >> std::hex >> target) {
-		if (0 < table[addr % table_size] && table[addr % table_size] <= 3 && behavior == "NT") {
-				if(table[addr % table_size] < 2){
+			if (0 < table[addr % table_size] && table[addr % table_size] <= 3 && behavior == "NT") {
+				if (table[addr % table_size] < 2) {
 					correct++;
 				}
 				table[addr % table_size]--;
 			}
 			else if (0 <= table[addr % table_size] && table[addr % table_size] < 3 && behavior == "T") {
-				if(table[addr % table_size] >= 2){
+				if (table[addr % table_size] >= 2) {
 					correct++;
 				}
 				table[addr % table_size]++;
@@ -113,13 +115,15 @@ void bimodal_2bit(string file_name) {
 }
 
 void gshare(string file_name) {
-	int correct = 0;
-	int total = 0;
-	for(int i = 2; i < 11; i++){
+	int correct;
+	int total;
+	for (int i = 2; i < 11; i++) {
 		unsigned short extractor = 0;
-		for(int j = 0; j <= i; j++){
+		for (int j = 0; j <= i; j++) {
 			extractor = (extractor << 1) | 1;
 		}
+		correct = 0;
+		total = 0;
 		int ghr = 0;
 		vector<short> table;
 		table.resize(2048, 3);
@@ -128,9 +132,27 @@ void gshare(string file_name) {
 		unsigned long long target;
 		ifstream infile(file_name);
 		while (infile >> std::hex >> addr >> behavior >> std::hex >> target) {
-			int index = (addr & extractor) ^ (ghr & extractor);
-			cout << index << endl;
+			int index = (addr % 2048) ^ (ghr & extractor);
+			(behavior == "T") ? ghr = (ghr << 1 | 1) : ghr = ghr << 1;
+			if (0 < table[index] && table[index] <= 3 && behavior == "NT") {
+				if (table[index] < 2) {
+					correct++;
+				}
+				table[index]--;
+			}
+			else if (0 <= table[index] && table[index] < 3 && behavior == "T") {
+				if (table[index] >= 2) {
+					correct++;
+				}
+				table[index]++;
+			}
+			else {
+				correct++;
+			}
+			total++;
 		}
+		cout << correct << "," << total << "; ";
+		infile.close();
 	}
 }
 
@@ -143,9 +165,9 @@ void branch_target_buffer() {
 }
 
 void test_mod(string file_name) {
-	for(int i = 2; i < 11; i++){
+	for (int i = 2; i < 11; i++) {
 		unsigned short extractor = 0;
-		for(int j = 0; j <= i; j++){
+		for (int j = 0; j <= i; j++) {
 			extractor = extractor << 1 | 1;
 		}
 		cout << extractor << endl;
